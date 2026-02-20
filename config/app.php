e6fb2a213e42d9a4dbdeef58ee8e852a85bbeccf
Devon Kiss <devon@3dmandt.com>
2026-02-20 01:34:52 -0600

    setup: configuring various portions of the app to have the proper name
    - Laravel implemented several ways to have the name of the application
      named different basd on some weird, in my mind, ways to setup the
      app's name with a default of Laravel.
    - I took all the Laravel backup mentions out of the codebase.
    - I don't like how Laravel chose to make this work at all.

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Devon\'s Software Engineering Portfolio'),
+    'name' => env('APP_NAME', 'Devon Kiss: Software Engineering Portfolio'),
 
     /*
     |--------------------------------------------------------------------------
