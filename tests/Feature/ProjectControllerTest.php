3f96a698c4489244573925bab277aa5adfa175bc
Devon Kiss <devon@3dmandt.com>
2026-02-25 12:18:15 -0600

    test: added test for the admiinistrative user creation of projects and no non-authorized user creation

--- tests/Feature/ProjectControllerTest.php
+++ tests/Feature/ProjectControllerTest.php
@@ -6,6 +6,59 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 uses(RefreshDatabase::class);
 
+test('authorized users can create projects', function () {
+  $admin = User::factory()->create(['is_admin' => true]);
+
+  $projectData = [
+    'title' => "Controller Test Project",
+    'slug' => "controller-test-project",
+    'description' => "Controller Test Project regular Description",
+    'short_description' => "Short Controller Test Description",
+    'url' => "https://controller-test-project.com/",
+    'github_url' => 'https://github.com/tuffs/controller-test-project',
+    'image_path' => '/projects/my-controller-test-project.png',
+    'featured' => false,
+    'order' => 0,
+    'video_url' => 'https://youtube.com/?w=2342334234',
+  ];
+
+  $response = $this
+    ->actingAs($admin)
+    ->post(route('projects.store'), $projectData);
+
+  $response->assertRedirect(route('projects.index'));
+  $this->assertDatabaseHas('projects', [
+    'title' => "Controller Test Project",
+    'user_id' => $admin->id,
+  ]);
+});
+
+test('unauthorized users can not create projects', function () {
+  $user = User::factory()->create(['is_admin' => false]);
+
+  $projectData = [
+    'title' => "Controller Test Project",
+    'slug' => "controller-test-project",
+    'description' => "Controller Test Project regular Description",
+    'short_description' => "Short Controller Test Description",
+    'url' => "https://controller-test-project.com/",
+    'github_url' => 'https://github.com/tuffs/controller-test-project',
+    'image_path' => '/projects/my-controller-test-project.png',
+    'featured' => false,
+    'order' => 0,
+    'video_url' => 'https://youtube.com/?w=2342334234',
+  ];
+
+  $response = $this
+    ->actingAs($user)
+    ->post(route('projects.store'), $projectData);
+
+  $response->assertStatus(403);
+  $this->assertDatabaseMissing('projects', [
+    'title' => "Controller Test Project",
+  ]);
+});
+
 test('authorized users can delete projects', function () {
   $user = User::factory()->create([
     'is_admin' => true
